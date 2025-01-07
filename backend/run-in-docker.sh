3b53ebe588a118eea28a2b45b39502380d855256
fenghao <moryaden@qq.com>
2025-01-07 23:47:10 +0800

    CI

---
+++ backend/run-in-docker.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd $(dirname $0)
+
+image=registry.cn-wulanchabu.aliyuncs.com/gouboyun-pub/python:3.11-slim-bookworm
+
+
+docker run -it --rm -v `pwd`:/app -w /app $image bash
+
+# cp .docker/sources.list /etc/apt/sources.list
+    # apt-get install -y --no-install-recommends git build-essential pandoc gcc netcat-openbsd curl jq && \
+    # apt-get install -y --no-install-recommends gcc python3-dev && \
+    # # for RAG OCR
+    # apt-get install -y --no-install-recommends ffmpeg libsm6 libxext6 && \
+    # # cleanup
+    # rm -rf /var/lib/apt/lists/*;
+
+# pip config set global.index-url https://mirrors.aliyun.com/pypi/simple/
+# pip install -r requirements.txt
