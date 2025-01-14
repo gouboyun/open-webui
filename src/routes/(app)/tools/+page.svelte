eddfc0acbbaf1ced44b8166dd31ae92cc5483db1
zxk <zxk5133@163.com>
2025-01-14 14:49:17 +0800

    fix

--- src/routes/(app)/tools/+page.svelte
+++ src/routes/(app)/tools/+page.svelte
@@ -10,7 +10,7 @@
 		{
 			icon: 'carbon:report',
 			title: '周报生成',
-			link:'/tools/weekreport'
+			link: '/tools/weekreport'
 		},
 		{
 			icon: 'carbon:report',
@@ -38,21 +38,26 @@
 		}
 	];
 
-	const toPage = (path)=>{
-		goto(path)
-	}
+	const toPage = (path) => {
+		goto(path);
+	};
 </script>
 
 <div class="  w-full bg-[#efefef] grid grid-cols-6 grid-rows-4">
 	{#each tools as tool}
-		<button  on:click={()=>{
-			toPage(tool.link)
-		}} class=" hover:scale-110  flex flex-col justify-center items-center  h-40">
-			<div class=" w-20 h-20 rounded-full bg-white shadow flex items-center justify-center">
+		<button
+			on:click={() => {
+				toPage(tool.link);
+			}}
+			class="   flex flex-col justify-center items-center h-40"
+		>
+			<div
+				class="hover:scale-110 w-20 h-20 rounded-full bg-white shadow flex items-center justify-center"
+			>
 				<Icon icon={tool.icon} font-size={40} color={'#aaa'} />
 			</div>
 
-			<div class="text-[1rem] font-bold mt-4  font-primary">{tool.title}</div>
+			<div class="text-[1rem] font-bold mt-4 font-primary">{tool.title}</div>
 		</button>
 	{/each}
 </div>
