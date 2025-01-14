3d506c2408dc0527b46479fd1b139afec576db60
zxk <zxk5133@163.com>
2025-01-14 15:28:51 +0800

    fix: ai chat bar 抽取

---
+++ src/lib/components/layout/Sidebar/ChatBar.svelte
@@ -0,0 +1,507 @@
+<script lang="ts">
+	import { getAllTags, getChatById, getChatList, getChatListBySearchText, getPinnedChatList, importChat, toggleChatPinnedStatusById, updateChatFolderIdById } from "$lib/apis/chats";
+	import { createNewFolder, getFolders, updateFolderParentIdById } from "$lib/apis/folders";
+	import { channels, chats, config, currentChatPage, pinnedChats, scrollPaginationEnabled, showArchivedChats, tags, temporaryChatEnabled, user } from "$lib/stores";
+	import { Loader, toast } from "svelte-sonner";
+	import SearchInput from "./SearchInput.svelte";
+	import { getContext } from "svelte";
+	import Folder from "$lib/components/common/Folder.svelte";
+	import ChannelItem from "./ChannelItem.svelte";
+	import { getChannels } from "$lib/apis/channels";
+    import { v4 as uuidv4 } from 'uuid';
+	import ChatItem from "./ChatItem.svelte";
+	import Folders from "./Folders.svelte";
+	import Spinner from "$lib/components/common/Spinner.svelte";
+	import UserMenu from "./UserMenu.svelte";
+    
+    const i18n = getContext('i18n');
+    
+	let search = '';
+    let selectedChatId = null;
+    let folders = {};
+
+    let shiftKey = false;
+    let showDropdown = false;
+    let showCreateChannel = false;
+    let showPinnedChat = true;
+    // Pagination variables
+	let chatListLoading = false;
+	let allChatsLoaded = false;
+
+    let searchDebounceTimeout;
+    
+
+    const tagEventHandler = async (type, tagName, chatId) => {
+		console.log(type, tagName, chatId);
+		if (type === 'delete') {
+			initChatList();
+		} else if (type === 'add') {
+			initChatList();
+		}
+	};
+
+    
+    const searchDebounceHandler = async () => {
+		console.log('search', search);
+		chats.set(null);
+
+		if (searchDebounceTimeout) {
+			clearTimeout(searchDebounceTimeout);
+		}
+
+		if (search === '') {
+			await initChatList();
+			return;
+		} else {
+			searchDebounceTimeout = setTimeout(async () => {
+				allChatsLoaded = false;
+				currentChatPage.set(1);
+				await chats.set(await getChatListBySearchText(localStorage.token, search));
+
+				if ($chats.length === 0) {
+					tags.set(await getAllTags(localStorage.token));
+				}
+			}, 1000);
+		}
+	};
+
+
+	const initFolders = async () => {
+		const folderList = await getFolders(localStorage.token).catch((error) => {
+			toast.error(error);
+			return [];
+		});
+
+		folders = {};
+
+		// First pass: Initialize all folder entries
+		for (const folder of folderList) {
+			// Ensure folder is added to folders with its data
+			folders[folder.id] = { ...(folders[folder.id] || {}), ...folder };
+		}
+
+		// Second pass: Tie child folders to their parents
+		for (const folder of folderList) {
+			if (folder.parent_id) {
+				// Ensure the parent folder is initialized if it doesn't exist
+				if (!folders[folder.parent_id]) {
+					folders[folder.parent_id] = {}; // Create a placeholder if not already present
+				}
+
+				// Initialize childrenIds array if it doesn't exist and add the current folder id
+				folders[folder.parent_id].childrenIds = folders[folder.parent_id].childrenIds
+					? [...folders[folder.parent_id].childrenIds, folder.id]
+					: [folder.id];
+
+				// Sort the children by updated_at field
+				folders[folder.parent_id].childrenIds.sort((a, b) => {
+					return folders[b].updated_at - folders[a].updated_at;
+				});
+			}
+		}
+	};
+
+    const createFolder = async (name = 'Untitled') => {
+		if (name === '') {
+			toast.error($i18n.t('Folder name cannot be empty.'));
+			return;
+		}
+
+		const rootFolders = Object.values(folders).filter((folder) => folder.parent_id === null);
+		if (rootFolders.find((folder) => folder.name.toLowerCase() === name.toLowerCase())) {
+			// If a folder with the same name already exists, append a number to the name
+			let i = 1;
+			while (
+				rootFolders.find((folder) => folder.name.toLowerCase() === `${name} ${i}`.toLowerCase())
+			) {
+				i++;
+			}
+
+			name = `${name} ${i}`;
+		}
+
+		// Add a dummy folder to the list to show the user that the folder is being created
+		const tempId = uuidv4();
+		folders = {
+			...folders,
+			tempId: {
+				id: tempId,
+				name: name,
+				created_at: Date.now(),
+				updated_at: Date.now()
+			}
+		};
+
+		const res = await createNewFolder(localStorage.token, name).catch((error) => {
+			toast.error(error);
+			return null;
+		});
+
+		if (res) {
+			await initFolders();
+		}
+	};
+
+    const initChatList = async () => {
+		// Reset pagination variables
+		tags.set(await getAllTags(localStorage.token));
+		pinnedChats.set(await getPinnedChatList(localStorage.token));
+		initFolders();
+
+		currentChatPage.set(1);
+		allChatsLoaded = false;
+
+		if (search) {
+			await chats.set(await getChatListBySearchText(localStorage.token, search, $currentChatPage));
+		} else {
+			await chats.set(await getChatList(localStorage.token, $currentChatPage));
+		}
+
+		// Enable pagination
+		scrollPaginationEnabled.set(true);
+	};
+
+    const importChatHandler = async (items, pinned = false, folderId = null) => {
+		console.log('importChatHandler', items, pinned, folderId);
+		for (const item of items) {
+			console.log(item);
+			if (item.chat) {
+				await importChat(localStorage.token, item.chat, item?.meta ?? {}, pinned, folderId);
+			}
+		}
+
+		initChatList();
+	};
+    
+	const initChannels = async () => {
+		await channels.set(await getChannels(localStorage.token));
+	};
+
+    const loadMoreChats = async () => {
+		chatListLoading = true;
+
+		currentChatPage.set($currentChatPage + 1);
+
+		let newChatList = [];
+
+		if (search) {
+			newChatList = await getChatListBySearchText(localStorage.token, search, $currentChatPage);
+		} else {
+			newChatList = await getChatList(localStorage.token, $currentChatPage);
+		}
+
+		// once the bottom of the list has been reached (no results) there is no need to continue querying
+		allChatsLoaded = newChatList.length === 0;
+		await chats.set([...($chats ? $chats : []), ...newChatList]);
+
+		chatListLoading = false;
+	};
+    
+</script>
+
+
+<div class="relative {$temporaryChatEnabled ? 'opacity-20' : ''}">
+    {#if $temporaryChatEnabled}
+        <div class="absolute z-40 w-full h-full flex justify-center"></div>
+    {/if}
+
+    <SearchInput
+        bind:value={search}
+        on:input={searchDebounceHandler}
+        placeholder={$i18n.t('Search')}
+    />
+</div>
+
+<div
+    class="relative flex flex-col flex-1 overflow-y-auto overflow-x-hidden {$temporaryChatEnabled
+        ? 'opacity-20'
+        : ''}"
+>
+    {#if $config?.features?.enable_channels && ($user.role === 'admin' || $channels.length > 0) && !search}
+        <Folder
+            className="px-2 mt-0.5"
+            name={$i18n.t('Channels')}
+            dragAndDrop={false}
+            onAdd={$user.role === 'admin'
+                ? () => {
+                        showCreateChannel = true;
+                    }
+                : null}
+            onAddLabel={$i18n.t('Create Channel')}
+        >
+            {#each $channels as channel}
+                <ChannelItem
+                    {channel}
+                    onUpdate={async () => {
+                        await initChannels();
+                    }}
+                />
+            {/each}
+        </Folder>
+    {/if}
+
+    <Folder
+        collapsible={!search}
+        className="px-2 mt-0.5"
+        name={$i18n.t('Chats')}
+        onAdd={() => {
+            createFolder();
+        }}
+        onAddLabel={$i18n.t('New Folder')}
+        on:import={(e) => {
+            importChatHandler(e.detail);
+        }}
+        on:drop={async (e) => {
+            const { type, id, item } = e.detail;
+
+            if (type === 'chat') {
+                let chat = await getChatById(localStorage.token, id).catch((error) => {
+                    return null;
+                });
+                if (!chat && item) {
+                    chat = await importChat(localStorage.token, item.chat, item?.meta ?? {});
+                }
+
+                if (chat) {
+                    console.log(chat);
+                    if (chat.folder_id) {
+                        const res = await updateChatFolderIdById(localStorage.token, chat.id, null).catch(
+                            (error) => {
+                                toast.error(error);
+                                return null;
+                            }
+                        );
+                    }
+
+                    if (chat.pinned) {
+                        const res = await toggleChatPinnedStatusById(localStorage.token, chat.id);
+                    }
+
+                    initChatList();
+                }
+            } else if (type === 'folder') {
+                if (folders[id].parent_id === null) {
+                    return;
+                }
+
+                const res = await updateFolderParentIdById(localStorage.token, id, null).catch(
+                    (error) => {
+                        toast.error(error);
+                        return null;
+                    }
+                );
+
+                if (res) {
+                    await initFolders();
+                }
+            }
+        }}
+    >
+        {#if $temporaryChatEnabled}
+            <div class="absolute z-40 w-full h-full flex justify-center"></div>
+        {/if}
+
+        {#if !search && $pinnedChats.length > 0}
+            <div class="flex flex-col space-y-1 rounded-xl">
+                <Folder
+                    className=""
+                    bind:open={showPinnedChat}
+                    on:change={(e) => {
+                        localStorage.setItem('showPinnedChat', e.detail);
+                        console.log(e.detail);
+                    }}
+                    on:import={(e) => {
+                        importChatHandler(e.detail, true);
+                    }}
+                    on:drop={async (e) => {
+                        const { type, id, item } = e.detail;
+
+                        if (type === 'chat') {
+                            let chat = await getChatById(localStorage.token, id).catch((error) => {
+                                return null;
+                            });
+                            if (!chat && item) {
+                                chat = await importChat(localStorage.token, item.chat, item?.meta ?? {});
+                            }
+
+                            if (chat) {
+                                console.log(chat);
+                                if (chat.folder_id) {
+                                    const res = await updateChatFolderIdById(
+                                        localStorage.token,
+                                        chat.id,
+                                        null
+                                    ).catch((error) => {
+                                        toast.error(error);
+                                        return null;
+                                    });
+                                }
+
+                                if (!chat.pinned) {
+                                    const res = await toggleChatPinnedStatusById(localStorage.token, chat.id);
+                                }
+
+                                initChatList();
+                            }
+                        }
+                    }}
+                    name={$i18n.t('Pinned')}
+                >
+                    <div
+                        class="ml-3 pl-1 mt-[1px] flex flex-col overflow-y-auto scrollbar-hidden border-s border-gray-100 dark:border-gray-900"
+                    >
+                        {#each $pinnedChats as chat, idx}
+                            <ChatItem
+                                className=""
+                                id={chat.id}
+                                title={chat.title}
+                                {shiftKey}
+                                selected={selectedChatId === chat.id}
+                                on:select={() => {
+                                    selectedChatId = chat.id;
+                                }}
+                                on:unselect={() => {
+                                    selectedChatId = null;
+                                }}
+                                on:change={async () => {
+                                    initChatList();
+                                }}
+                                on:tag={(e) => {
+                                    const { type, name } = e.detail;
+                                    tagEventHandler(type, name, chat.id);
+                                }}
+                            />
+                        {/each}
+                    </div>
+                </Folder>
+            </div>
+        {/if}
+
+        {#if !search && folders}
+            <Folders
+                {folders}
+                on:import={(e) => {
+                    const { folderId, items } = e.detail;
+                    importChatHandler(items, false, folderId);
+                }}
+                on:update={async (e) => {
+                    initChatList();
+                }}
+                on:change={async () => {
+                    initChatList();
+                }}
+            />
+        {/if}
+
+        <div class=" flex-1 flex flex-col overflow-y-auto scrollbar-hidden">
+            <div class="pt-1.5">
+                {#if $chats}
+                    {#each $chats as chat, idx}
+                        {#if idx === 0 || (idx > 0 && chat.time_range !== $chats[idx - 1].time_range)}
+                            <div
+                                class="w-full pl-2.5 text-xs text-gray-500 dark:text-gray-500 font-medium {idx ===
+                                0
+                                    ? ''
+                                    : 'pt-5'} pb-1.5"
+                            >
+                                {$i18n.t(chat.time_range)}
+                                <!-- localisation keys for time_range to be recognized from the i18next parser (so they don't get automatically removed):
+                    {$i18n.t('Today')}
+                    {$i18n.t('Yesterday')}
+                    {$i18n.t('Previous 7 days')}
+                    {$i18n.t('Previous 30 days')}
+                    {$i18n.t('January')}
+                    {$i18n.t('February')}
+                    {$i18n.t('March')}
+                    {$i18n.t('April')}
+                    {$i18n.t('May')}
+                    {$i18n.t('June')}
+                    {$i18n.t('July')}
+                    {$i18n.t('August')}
+                    {$i18n.t('September')}
+                    {$i18n.t('October')}
+                    {$i18n.t('November')}
+                    {$i18n.t('December')}
+                    -->
+                            </div>
+                        {/if}
+
+                        <ChatItem
+                            className=""
+                            id={chat.id}
+                            title={chat.title}
+                            {shiftKey}
+                            selected={selectedChatId === chat.id}
+                            on:select={() => {
+                                selectedChatId = chat.id;
+                            }}
+                            on:unselect={() => {
+                                selectedChatId = null;
+                            }}
+                            on:change={async () => {
+                                initChatList();
+                            }}
+                            on:tag={(e) => {
+                                const { type, name } = e.detail;
+                                tagEventHandler(type, name, chat.id);
+                            }}
+                        />
+                    {/each}
+
+                    {#if $scrollPaginationEnabled && !allChatsLoaded}
+                        <Loader
+                            on:visible={(e) => {
+                                if (!chatListLoading) {
+                                    loadMoreChats();
+                                }
+                            }}
+                        >
+                            <div
+                                class="w-full flex justify-center py-1 text-xs animate-pulse items-center gap-2"
+                            >
+                                <Spinner className=" size-4" />
+                                <div class=" ">Loading...</div>
+                            </div>
+                        </Loader>
+                    {/if}
+                {:else}
+                    <div class="w-full flex justify-center py-1 text-xs animate-pulse items-center gap-2">
+                        <Spinner className=" size-4" />
+                        <div class=" ">Loading...</div>
+                    </div>
+                {/if}
+            </div>
+        </div>
+    </Folder>
+</div>
+
+<div class="px-2">
+    <div class="flex flex-col font-primary">
+        {#if $user !== undefined}
+            <UserMenu
+                role={$user.role}
+                on:show={(e) => {
+                    if (e.detail === 'archived-chat') {
+                        showArchivedChats.set(true);
+                    }
+                }}
+            >
+                <button
+                    class=" flex items-center rounded-xl py-2.5 px-2.5 w-full hover:bg-gray-100 dark:hover:bg-gray-900 transition"
+                    on:click={() => {
+                        showDropdown = !showDropdown;
+                    }}
+                >
+                    <div class=" self-center mr-3">
+                        <img
+                            src={$user.profile_image_url}
+                            class=" max-w-[30px] object-cover rounded-full"
+                            alt="User profile"
+                        />
+                    </div>
+                    <div class=" self-center font-medium">{$user.name}</div>
+                </button>
+            </UserMenu>
+        {/if}
+    </div>
+</div>
