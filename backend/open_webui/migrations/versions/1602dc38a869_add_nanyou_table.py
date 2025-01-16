c8d658cadac0f04d0c06510c53a4b9f8420a5b14
fenghao <moryaden@qq.com>
2025-01-16 18:44:52 +0800

    add migrate

---
+++ backend/open_webui/migrations/versions/1602dc38a869_add_nanyou_table.py
@@ -0,0 +1,36 @@
+"""Add config table
+
+Revision ID: 1602dc38a869
+Revises: 3781e22d8b01
+Create Date: 2024-08-25 15:26:35.241684
+
+"""
+
+from typing import Sequence, Union
+
+import sqlalchemy as sa
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "1602dc38a869"
+down_revision: Union[str, None] = "3781e22d8b01"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade():
+    op.create_table(
+        "pdftable",
+        sa.Column("id", sa.Text(), primary_key=True),
+        sa.Column("user_id", sa.Text(), nullable=False),
+        sa.Column("name", sa.Text(), nullable=False),
+        sa.Column("description", sa.Text(), nullable=True),
+        sa.Column("data", sa.JSON(), nullable=True),
+        sa.Column("meta", sa.JSON(), nullable=True),
+        sa.Column("created_at", sa.BigInteger(), nullable=False),
+        sa.Column("updated_at", sa.BigInteger(), nullable=True),
+    )
+
+
+def downgrade():
+    op.drop_table("pdftable")
