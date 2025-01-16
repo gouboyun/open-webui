d2b63370226ea88e2d876a763dffcd42243a61d6
zxk <zxk5133@163.com>
2025-01-16 20:45:27 +0800

    fix

--- src/lib/components/layout/Sidebar/Pdf.svelte
+++ src/lib/components/layout/Sidebar/Pdf.svelte
@@ -2,21 +2,23 @@
 	import { getContext } from 'svelte';
 	import SearchInput from './SearchInput.svelte';
 	import DocumentArrowUpSolid from '$lib/components/icons/DocumentArrowUpSolid.svelte';
-	import { config , user as _user } from '$lib/stores';
-    import { v4 as uuidv4 } from 'uuid';
+	import { config, user as _user } from '$lib/stores';
+	import { v4 as uuidv4 } from 'uuid';
 	import { toast } from 'svelte-sonner';
 	import { WEBUI_API_BASE_URL } from '$lib/constants';
 	import { transcribeAudio } from '$lib/apis/audio';
 	import { blobToFile } from '$lib/utils';
 	import { uploadFile } from '$lib/apis/files';
 	import FileItem from '$lib/components/common/FileItem.svelte';
+	import { addFileToPdfById } from '$lib/apis/pdf';
+
+	import AddContentMenu from '$lib/components/pdf/AddContentMenu.svelte';
 	const i18n = getContext('i18n');
 	let filesInputElement;
-    let inputFiles;
-    let files = [];
+	let inputFiles;
+	let files = [];
 
-
-    const uploadFileHandler = async (file, fullContext: boolean = false) => {
+	const uploadFileHandler = async (file, fullContext: boolean = false) => {
 		if ($_user?.role !== 'admin' && !($_user?.permissions?.chat?.file_upload ?? true)) {
 			toast.error($i18n.t('You do not have permission to upload files.'));
 			return null;
@@ -64,6 +66,7 @@
 			const uploadedFile = await uploadFile(localStorage.token, file);
 
 			if (uploadedFile) {
+				debugger;
 				console.log('File upload completed:', {
 					id: uploadedFile.id,
 					name: fileItem.name,
@@ -74,29 +77,43 @@
 					console.warn('File upload warning:', uploadedFile.error);
 					toast.warning(uploadedFile.error);
 				}
+
+				await addFileHandler(uploadedFile.id);
 				// 文件上传成功后，自动触发pdf文件内容预览
-				
 
-				fileItem.status = 'uploaded';
-				fileItem.file = uploadedFile;
-				fileItem.id = uploadedFile.id;
-				fileItem.collection_name =
-					uploadedFile?.meta?.collection_name || uploadedFile?.collection_name;
-				fileItem.url = `${WEBUI_API_BASE_URL}/files/${uploadedFile.id}`;
+				// fileItem.status = 'uploaded';
+				// fileItem.file = uploadedFile;
+				// fileItem.id = uploadedFile.id;
+				// fileItem.collection_name =
+				// 	uploadedFile?.meta?.collection_name || uploadedFile?.collection_name;
+				// fileItem.url = `${WEBUI_API_BASE_URL}/files/${uploadedFile.id}`;
 
-				files = files;
+				//files = files;
 			} else {
-				files = files.filter((item) => item?.itemId !== tempItemId);
+				//files = files.filter((item) => item?.itemId !== tempItemId);
 			}
 		} catch (e) {
 			toast.error(e);
-			files = files.filter((item) => item?.itemId !== tempItemId);
+			//files = files.filter((item) => item?.itemId !== tempItemId);
 		}
 	};
 
+	const addFileHandler = async (fileId) => {
+		const updatedKnowledge = await addFileToPdfById(localStorage.token, id, fileId).catch((e) => {
+			toast.error(e);
+			return null;
+		});
 
+		// if (updatedKnowledge) {
+		// 	knowledge = updatedKnowledge;
+		// 	toast.success($i18n.t('File added successfully.'));
+		// } else {
+		// 	toast.error($i18n.t('Failed to add file.'));
+		// 	knowledge.files = knowledge.files.filter((file) => file.id !== fileId);
+		// }
+	};
 
-    const inputFilesHandler = async (_files) => {
+	const inputFilesHandler = async (_files) => {
 		console.log('Input files handler called with:', _files);
 		_files.forEach((file) => {
 			console.log('Processing file:', {
@@ -150,36 +167,22 @@
 			// 	};
 			// 	reader.readAsDataURL(file);
 			// } else {
-				uploadFileHandler(file);
-		    //}
+			uploadFileHandler(file);
+			//}
 		});
 	};
-
-    
-
 </script>
 
 <div class="relative">
-    <div>
-        <button
-            class="flex w-full mx-2 items-center px-3 py-2 text-sm font-medium cursor-pointer hover:bg-gray-100 dark:hover:bg-gray-800 rounded-xl"
-            on:click={() => {
-                filesInputElement.click();
-            }}
-        >
-            <DocumentArrowUpSolid />
-            <div class="line-clamp-1">{$i18n.t('Upload Files')}</div>
-        </button>
-    </div>
-	
+
 	<input
 		bind:this={filesInputElement}
-        bind:files={inputFiles}
+		bind:files={inputFiles}
 		type="file"
 		hidden
 		multiple={false}
 		on:change={async () => {
-            console.log(inputFiles)
+			console.log(inputFiles);
 			if (inputFiles && inputFiles.length > 0) {
 				const _inputFiles = Array.from(inputFiles);
 				inputFilesHandler(_inputFiles);
@@ -190,9 +193,71 @@
 		}}
 	/>
 
-	<SearchInput placeholder={$i18n.t('Search')} />
-    
-    <!-- <FileItem
+	<div
+		class="flex-shrink-0 w-full
+			flex
+			py-2
+			rounded-2xl
+			border
+			border-gray-50
+			h-full
+			dark:border-gray-850"
+	>
+		<div class=" flex flex-col w-full space-x-2 rounded-lg h-full">
+			<div class="w-full h-full flex flex-col">
+				<div class=" pr-3">
+					<div class="flex mb-0.5">
+						<SearchInput placeholder={$i18n.t('Search')} />
+						<div>
+							<AddContentMenu
+								on:upload={(e) => {
+									// if (e.detail.type === 'directory') {
+									// 	uploadDirectoryHandler();
+									// } else if (e.detail.type === 'text') {
+									// 	showAddTextContentModal = true;
+									// } else {
+									// 	document.getElementById('files-input').click();
+									// }
+								}}
+								on:sync={(e) => {
+									//showSyncConfirmModal = true;
+								}}
+							/>
+						</div>
+					</div>
+				</div>
+
+				<!-- {#if filteredItems.length > 0}
+					<div class=" flex overflow-y-auto h-full w-full scrollbar-hidden text-xs">
+						<Files
+							small
+							files={filteredItems}
+							{selectedFileId}
+							on:click={(e) => {
+								selectedFileId = selectedFileId === e.detail ? null : e.detail;
+							}}
+							on:delete={(e) => {
+								console.log(e.detail);
+
+								selectedFileId = null;
+								deleteFileHandler(e.detail);
+							}}
+						/>
+					</div>
+				{:else}
+					<div class="my-3 flex flex-col justify-center text-center text-gray-500 text-xs">
+						<div>
+							{$i18n.t('No content found')}
+						</div>
+					</div>
+				{/if} -->
+			</div>
+		</div>
+	</div>
+
+	
+
+	<!-- <FileItem
         item={file}
         url={file.url}
         name={file.name}
