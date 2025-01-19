574e519301bb24722ed8ac740dc34c5c0eb43ad8
zxk <zxk5133@163.com>
2025-01-19 13:28:50 +0800

    fix

--- src/routes/(app)/tools/weekreport/+page.svelte
+++ src/routes/(app)/tools/weekreport/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
+	import Chat from '$lib/components/chat/Chat.svelte';
+	
+	import Help from '$lib/components/layout/Help.svelte';
 </script>
-
-<div class="  w-full ">
-	周报生成
-</div>
+<Help />
+<Chat />
